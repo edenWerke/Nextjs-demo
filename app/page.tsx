70faf4bc4cb6369096b07febb9d98eeaf4cabfb2
edenWerke <edenw6292@gmail.com>
2025-04-02 20:50:34 +0300

    page and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import {Greet} from './components/Greet';
-// Uncomment the following line if you want to use the Counter component
-import Counter from './components/Counter';
+// import {Greet} from './components/Greet';
+// // Uncomment the following line if you want to use the Counter component
+// import Counter from './components/Counter';
 
-export default function Home() {
-  return (
-    <main>
-      <Greet />
-      {/* Uncomment the following line if you want to use the Counter component */}
-      <Counter />
-    </main>
-  );
+// export default function Home() {
+//   return (
+//     <main>
+//       <Greet />
+//       {/* Uncomment the following line if you want to use the Counter component */}
+//       <Counter />
+//     </main>
+//   );
+// }
+export default function Page(){
+  return <h1>Hello next.jsx</h1>
 }
