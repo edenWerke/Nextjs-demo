018f966b3a30333b39a9c55db360879f388034a8
edenWerke <edenw6292@gmail.com>
2025-04-03 11:14:47 +0300

    local time string

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,12 +5,13 @@ interface User{
 }
 
 const UserPage = async ()=>{
-const res= await fetch('https://jsonplaceholder.typicode.com/users',{next :{revalidate:10}})
+const res= await fetch('https://jsonplaceholder.typicode.com/users',{next :{revalidate:10 }})
 const users:User[]=await res.json();
 
     return (
         <>
         <h1>users i fetch</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
         <ol>
                 {users.map(user=><li key={user.id}>{user.name}</li>)}
             </ol>
