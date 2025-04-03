3166bf67a243e1f0c35902a08062001f2fac1100
edenWerke <edenw6292@gmail.com>
2025-04-03 11:08:23 +0300

    fetching data

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,7 +5,7 @@ interface User{
 }
 
 const UserPage = async ()=>{
-const res= await fetch('https://jsonplaceholder.typicode.com/users')
+const res= await fetch('https://jsonplaceholder.typicode.com/users',{cache:'no-store'})
 const users:User[]=await res.json();
 
     return (
