c74d809e5d425073f4d415ea69e2e196dc933954
edenWerke <edenw6292@gmail.com>
2025-04-03 11:04:18 +0300

    fetching data

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+interface User{
+    id:number;
+    name:string;
+}
 
-const UserPage =()=>{
+const UserPage = async ()=>{
+const res= await fetch('https://jsonplaceholder.typicode.com/users')
+const users:User[]=await res.json();
+
     return (
-        <div>User page</div>
+        <>
+        <h1>users i fetch</h1>
+        <ol>
+                {users.map(user=><li key={user.id}>{user.name}</li>)}
+            </ol>
+        
+        </>
     )
 }
 export default UserPage;
